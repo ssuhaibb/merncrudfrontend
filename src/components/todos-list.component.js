da52e39c1027a30e00d7eecfe822fa82def2c178
Mohammad Suhaib <ssuhaibb@gmail.com>
2019-03-06 17:25:19 +0530

    mern crud initial commit

---
+++ src/components/todos-list.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TodosList extends Component {
+    render() {
+        return (
+            <div>
+                <p>Welcome to ToDos List Component</p>
+            </div>
+        )
+    }
+}
